b277bfb49d8e69c87da77112aa0217430ffb7a1c
natilopera <natalopera28@gmail.com>
2022-03-02 17:34:13 -0500

    2marzo

---
+++ src/app/contador.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector: 'app-contador',
+template: `
+<h1>{{ titulo }}</h1>
+<h2>La base es <strong> {{ base }}</strong></h2>
+
+<button (click)="acumular(base)"> + {{ base }} </button>
+
+<span>{{ numero }}</span>
+
+<button (click)="acumular(base)"> - {{ base }} </button>
+`
+}) 
+
+export class ContadorComponent {
+    titulo: string = 'Sebas';
+    numero: number = 10;
+    base: number = 5;
+  acumular ( valor: number ) {
+    this.numero += valor; 
+    } 
+}
+    
